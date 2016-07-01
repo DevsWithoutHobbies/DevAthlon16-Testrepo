818d321dbab381ba0fd27c687eccce177e565a15
Jonas <jonas.it@icloud.com>
2016-07-01 21:29:21 +0200

    Created Plugin Structure

---
+++ Test/src/de/DevsWithoutHobbies/Test/Main.java
@@ -0,0 +1,12 @@
+package de.DevsWithoutHobbies.Test;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        System.out.println("Is jetzt da");
+    }
+
+}
