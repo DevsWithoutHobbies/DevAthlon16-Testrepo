07d6803bb3fdfbcc347725434e403f744baecbd9
Jonas <jonas.it@icloud.com>
2016-07-02 16:55:00 +0200

    Wool BLOCKS

--- Test/src/de/DevsWithoutHobbies/Test/EventListener.java
+++ Test/src/de/DevsWithoutHobbies/Test/EventListener.java
@@ -1,15 +1,20 @@
 package de.DevsWithoutHobbies.Test;
 
-import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerMoveEvent;
 
 class EventListener implements Listener {
-
     @EventHandler
     void playerMove(PlayerMoveEvent event) {
-        double distance = event.getFrom().getY() - event.getTo().getY();
-        Bukkit.broadcastMessage(event.getPlayer().getDisplayName() + " moved " + distance);
+        double distance = event.getTo().getY() - event.getFrom().getY();
+
+        if (distance < -0.2) {
+            Block block = event.getPlayer().getWorld().getBlockAt((int) event.getTo().getX(), (int) event.getTo().getY() - 1, (int) event.getTo().getZ());
+            block.setType(Material.WOOL);
+            block.setData((byte) (block.getY() % 16));
+        }
     }
 }
