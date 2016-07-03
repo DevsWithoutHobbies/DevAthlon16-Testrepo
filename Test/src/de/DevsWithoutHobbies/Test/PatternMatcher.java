01e57357480b6c0b405673c4e721617088d22721
Noah Peeters <noah.peeters@icloud.com>
2016-07-03 14:02:44 +0200

    Pattern Matcher + new commands

---
+++ Test/src/de/DevsWithoutHobbies/Test/PatternMatcher.java
@@ -0,0 +1,20 @@
+package de.DevsWithoutHobbies.Test;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Created by noah on 03/07/16.
+ */
+
+public class PatternMatcher {
+    public static boolean match(String pattern, String string) {
+        String new_pattern = pattern.replaceAll("[*]", "\\\\w*");
+        new_pattern = new_pattern.replaceAll("[?]", "\\\\w");
+        new_pattern = "\\A" + new_pattern.replaceAll("[#]", "\\\\d") + "\\z";
+        Pattern r = Pattern.compile(new_pattern);
+        Matcher m = r.matcher(string);
+
+        return m.find();
+    }
+}
